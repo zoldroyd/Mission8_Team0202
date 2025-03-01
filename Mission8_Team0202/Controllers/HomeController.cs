1e37947b968e6c1ba2730cd33340cd9fc0698af8
Zac Oldroyd <137462136+zoldroyd@users.noreply.github.com>
2025-02-28 17:49:35 -0700

    donew

--- Mission8_Team0202/Controllers/HomeController.cs
+++ Mission8_Team0202/Controllers/HomeController.cs
@@ -3,116 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Mission8_Team0202.Models;
 
-// namespace Mission8_Team0202.Controllers;
-//
-// public class HomeController : Controller
-// {
-//     
-//     // private TaskContext _context;
-//     //
-//     // public HomeController(TaskContext context) // constructor
-//     // {
-//     //     _context = context;
-//     // }
-//     
-//     private ITaskRepository _repo;
-//
-//     public HomeController(ITaskRepository temp)
-//     {
-//         _repo = temp;
-//     }
-//     
-//     public IActionResult Index()
-//     {
-//         var taskToDo = _repo.TasksToDo
-//             .Include(x => x.Category) // join the category names
-//             .Where(x => x.Completed == false)
-//             .ToList();
-//         
-//         return View(taskToDo);
-//     }
-//     
-//     [HttpGet]
-//     public IActionResult CreateTask()
-//     {
-//         ViewBag.Categories = _repo.Categories
-//             .OrderBy(c => c.CategoryName)
-//             .ToList();
-//         
-//         return View("CreateTask", new TaskToDo());
-//     }
-//     
-//     [HttpPost]
-//     public IActionResult CreateTask(TaskToDo response)
-//     {
-//         if (ModelState.IsValid)
-//         {
-//             _repo.Tasks.Add(response); // add response to database
-//             _repo.SaveChanges(); // save the changes
-//
-//             return RedirectToAction("Index"); // reload page so form is empty
-//         }
-//         else
-//         {
-//             // add view bag so its loads the data
-//             ViewBag.Categories = _repo.Categories
-//                 .OrderBy(c => c.CategoryName)
-//                 .ToList();
-//             
-//             return View(response);
-//         }
-//     }
-//     
-//     [HttpGet]
-//     public IActionResult Edit(int id)
-//     {
-//         // get specific record by id
-//         var recordToEdit = _repo.Tasks
-//             .Single(x => x.TaskId == id);
-//         
-//         // viewbag to load data
-//         ViewBag.Categories = _repo.Categories
-//             .OrderBy(c => c.CategoryName)
-//             .ToList();
-//         
-//         return View("CreateTask", recordToEdit);
-//     }
-//
-//     [HttpPost]
-//     public IActionResult Edit(TaskToDo updatedInfo)
-//     {
-//         if (ModelState.IsValid)
-//         {
-//             _repo.Update(updatedInfo);  // Update the movie record
-//             _repo.SaveChanges();  // Save changes to the database
-//         
-//             return RedirectToAction("Index");  // Redirect to movie list page
-//         }
-//
-//         // If model state is invalid, reload the categories and return the form
-//         ViewBag.Categories = _repo.Categories
-//             .OrderBy(c => c.CategoryName)
-//             .ToList();
-//     
-//         return View("CreateTask", updatedInfo);
-//     }
-//
-//     [HttpPost] 
-//     public IActionResult Delete(int id) 
-//     { 
-//         var recordToDelete = _repo.Tasks
-//             .SingleOrDefault(x => x.TaskId == id);
-//         
-//         if (recordToDelete != null) 
-//         { 
-//             _repo.Tasks.Remove(recordToDelete);
-//             _repo.SaveChanges();
-//         } 
-//         
-//         return RedirectToAction("Index"); // Redirects to the main list view
-//     }
-// }
-
 
 namespace Mission8_Team0202.Controllers
 {
@@ -182,8 +72,7 @@ namespace Mission8_Team0202.Controllers
             ViewBag.Categories = _repo.GetCategories();
             return View("CreateTask", updatedInfo);
         }
-
-        [HttpPost]
+        
         public IActionResult Delete(int id)
         {
             var recordToDelete = _repo.TasksToDo
--- Mission8_Team0202/Models/EFTaskRepository.cs
+++ Mission8_Team0202/Models/EFTaskRepository.cs
@@ -1,40 +1,4 @@
-// namespace Mission8_Team0202.Models
-// {
-//     // Implementing the ITaskRepository interface
-//     public class EFTaskRepository : ITaskRepository
-//     {
-//         private TaskContext _context;
-//
-//         // Constructor with dependency injection
-//         public EFTaskRepository(TaskContext temp)
-//         {
-//             _context = temp;
-//         }
-//         
-//         public List<Task> TasksTodo => _context.Tasks.ToList();
-//
-//         // AddTask method to add a new task
-//         public void AddTask(Task task)
-//         {
-//             _context.Add(task);
-//             _context.SaveChanges();
-//         }
-//
-//         // UpdateTask method to update an existing task
-//         public void UpdateTask(Task task)
-//         {
-//             _context.Update(task);
-//             _context.SaveChanges();
-//         }
-//
-//         // DeleteTask method to remove an existing task
-//         public void DeleteTask(Task task)
-//         {
-//             _context.Remove(task);
-//             _context.SaveChanges();
-//         }
-//     }
-// }
+
 
 using Microsoft.EntityFrameworkCore;
 
